bd5a3e1d0f4c8fb20053c56ccd57ec8ede562133
IrregularPersona <irregularpersona@gmail.com>
2025-03-06 12:53:34 +0700

    removed flask & react from non-flask tracking

--- backend/config/production.py
+++
@@ -1,8 +0,0 @@
-from .base import BaseConfig
-
-class ProductionConfig(BaseConfig):
-    DEBUG = False
-    TESTING = False
-
-    SESSION_COOKIE_HTTPONLY = True
-    REMEMBER_COOKIE_HTTPONLY = True
